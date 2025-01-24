2413586d85a5e861abdead6871ba35a72479d088
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2025-01-24 10:15:05 -0600

    Add queue test

---
+++ README.md
@@ -0,0 +1,12 @@
+# Shift Code Challenges
+
+Solutions for Shift's engineering challenges
+
+## Use
+
+Install dependencies and run tests
+
+```sh
+npm i
+npm run test
+```
---
+++ tests/queue.test.ts
@@ -0,0 +1,67 @@
+/**
+ * Link to challenge: https://shiftparadigm.slack.com/archives/C06R174JN6P/p1728676802694129
+ * Challenge Date: 2024-09-11
+ */
+class Queue {
+	queue: any[] = [];
+
+	Enqueue = async (item: any) => {
+		if (typeof item === "function") {
+			this.queue.push(await item());
+		} else {
+			this.queue.push(item);
+		}
+	};
+
+	Dequeue = (): any | undefined => {
+		return this.queue.shift();
+	};
+
+	Peek = (): any | undefined => {
+		return this.queue[0];
+	};
+
+	Count = (): number => {
+		return this.queue.length;
+	};
+}
+
+const INPUT = ["This", "little", "piggy", "ate", "roast", "beef"];
+const OUTPUT = "This little piggy ate roast beef";
+
+async function enqueue() {
+	const myQueue = new Queue();
+
+	await Promise.all(INPUT.map(async (item) => await myQueue.Enqueue(item)));
+
+	let queueOutput = "";
+	while (myQueue.Count() > 0) {
+		queueOutput += myQueue.Dequeue();
+		if (myQueue.Count() > 0) queueOutput += " ";
+	}
+	return queueOutput;
+}
+
+async function enqueueFunction() {
+	const myQueue = new Queue();
+	const toes = async (piggy: string) => piggy;
+
+	await Promise.all(
+		INPUT.map(async (item) => await myQueue.Enqueue(() => toes(item))),
+	);
+
+	let queueOutput = "";
+	while (myQueue.Count() > 0) {
+		queueOutput += myQueue.Dequeue();
+		if (myQueue.Count() > 0) queueOutput += " ";
+	}
+	return queueOutput;
+}
+
+test("enqueue", async () => {
+	expect(await enqueue()).toBe(OUTPUT);
+});
+
+test("enqueueFunction", async () => {
+	expect(await enqueue()).toBe(OUTPUT);
+});
